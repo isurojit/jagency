35b02c0e37fdafd949f1e88f58ea08d0c6f42d03
isurojit <iamsurojit@hotmail.com>
2024-08-03 19:59:52 +0530

    Added Fields

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const scriptURL =
   "https://script.google.com/macros/s/AKfycbwE0Yr9YrK_hmNHLVRxgNE-tNfxEpYQYssP388ELxTu0-59MUZsDx4dnRlJ4oNAWjYzzQ/exec";
 const form = document.forms["google-sheet"];
 
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   fetch(scriptURL, { method: "POST", body: new FormData(form) })
+//     .then((response) =>
+//       alert("Thanks for Contacting us..! We Will Contact You Soon...")
+//     )
+//     .catch((error) => console.error("Error!", error.message));
+// });
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) =>
-      alert("Thanks for Contacting us..! We Will Contact You Soon...")
-    )
+    .then((response) => {
+      alert("Thanks for Submitting The Form..! We Will Contact You Soon...");
+      setTimeout(() => {
+        window.location.reload(); // Reload the page after 10 seconds
+        console.log("working");
+      }, 10000);
+    })
     .catch((error) => console.error("Error!", error.message));
 });
 
